4be16abb8222bbbe752aa0c453f405d729f22ba2
彭熙 <692912901@qq.com>
2024-10-11 18:27:10 +0800

    配合css实现了轮播图处于第几页的显示，当前页的圆点为红色，其它页为灰色

--- js/index.js
+++ js/index.js
@@ -1,8 +1,17 @@
 const width = document.documentElement.clientWidth//获取网页宽度
 var prev = document.querySelector('.prev');//获取第一个.prev元素
 var next = document.querySelector('.next');
-
-if (width > 800) {
+var dot = document.getElementById('dots').getElementsByTagName('li');
+var dotlist = document.querySelector('#dots');
+function dotchange(p) {
+    for (i = 0; i < dot.length; i++) {
+        dot[i].style.backgroundColor = '';
+    }
+    if (p < dot.length) {
+        dot[p].style.backgroundColor = 'red';
+    }
+}
+if (width > 785) {
     var left = 0;//定义left
     var timer;//定义timer计时器
     run()//使用run函数
@@ -10,10 +19,12 @@ if (width > 800) {
         if (left <= -2400) {
             left = 0;
         }
+        var t = Math.floor(-left / 800);
         imagelist.style.marginLeft = left + "px";
         var n = (left % 800 == 0) ? n = 1200 : n = 10;
         left -= 10;
         timer = setTimeout(run, n);
+        dotchange(t);
     }//定义run函数
     function change(x) {
         let y = -x * 800;
@@ -35,9 +46,15 @@ if (width > 800) {
         }
         change(z);
     }
+    // dotlist.addEventListener("click", function (event) {
+    //     var ev = event.target;
+    //     let x = parseInt(ev.innerHTML, 10) - 1;
+    //     change(x);
+    //     dotchange(x);
+    // })
 
 }
-if (width <= 800) {
+if (width <= 785) {
     var left = 0;
     var timer;
     run()
@@ -45,10 +62,12 @@ if (width <= 800) {
         if (left <= -1800) {
             left = 0;
         }
+        var t = Math.floor(-left / 600);
         imagelist.style.marginLeft = left + "px";
         var n = (left % 600 == 0) ? n = 1200 : n = 10;
         left -= 10;
         timer = setTimeout(run, n);
+        dotchange(t);
     }
     function change(x) {
         let y = -x * 600;
